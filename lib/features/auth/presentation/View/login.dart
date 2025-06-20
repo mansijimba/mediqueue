3e240698a9259ef629cb1e5ca7f350c67d1b965b
Mansi <mansezimba14@gmail.com>
2025-06-20 12:49:57 +0545

    updated

--- lib/features/auth/presentation/View/login.dart
+++ lib/features/auth/presentation/View/login.dart
@@ -13,8 +13,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController(text: 'test@email.com');
-  final _passwordController = TextEditingController(text: 'password123');
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   void dispose() {
